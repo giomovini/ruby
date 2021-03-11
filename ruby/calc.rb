f8f7fefa47968b6138d7a47c610e4b34e52bb3ec
Vinicius Giomo <63116407+giomovini@users.noreply.github.com>
2021-03-11 17:21:09 -0300

    Add files via upload

---
+++ ruby/calc.rb
@@ -0,0 +1,7 @@
+print "Digite o primeiro número inteiro: "
+# .to_i transforma a string em um número inteiro
+numbe1 = gets.chomp.to_i
+print "digite o segundo número inteiro: "
+number2 = gets.chomp.to_i
+addition = numbe1 + number2
+puts "O resultado da adição entre os dois números é #{addition}"
---
+++ ruby/hello.rb
@@ -0,0 +1,13 @@
+# saída de dado
+print 'Digite seu nome: '
+
+# recebendo uma entrada do teclado
+name = gets.chomp
+
+print 'Digite seu sobrenome: '
+
+sobrenome = gets.chomp
+
+# saída utilizando puts
+# use códigos ruby dentro de um string com #{code}
+puts "helloo #{name} #{sobrenome}!"
